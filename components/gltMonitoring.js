6b313ab091911cd692a57da4655b3023cc43eef7
Fenja Pestke <fenja.pestke@gmail.com>
2022-12-05 16:37:42 +0100

    styled the table in gltmonitoring

--- components/gltMonitoring.js
+++ components/gltMonitoring.js
@@ -9,38 +9,66 @@ class GeoTechMonitoring extends HTMLElement {
                 font-family: 'Arial', sans-serif;
                 display: flex;
                 flex-direction: column;
-                background: white;
+                background: #F5F5F5;
                 border: black 2px solid;
                 margin-top:30px
             }
             .glt-wrapper-monitoring{
                 display: flex;
                 flex-direction: row;
-                justify-content: space-around 
+                justify-content: space-between;
+                margin: auto 1.5rem;
             }
             .glt-title-monitoring {
                 display: flex;
                 flex-direction: row;
+                justify-content: end;
+            }
+
+            .glt-monitoring-table {
+                margin: auto 1.5rem 1.5rem;
             }
             .glt-monitoring img {
                 width: 8%;
             }
             table {
                 font-family: arial, sans-serif;
-                border-collapse: collapse;
+                border-collapse: seperate;
+                border-spacing: 0 10px;
                 width: 100%;
             }
-              
+            
             td, th {
-                border: 1px solid #dddddd;
                 text-align: left;
-                padding: 8px;
+                padding: 16px;
+                border-top: 1px solid white;
+                border-bottom: 1px solid white;
+            }
+
+            td:first-child {
+                border-left: 1px solid white;
+            }
+
+            td:last-child {
+                border-right: 1px solid white;
+            }
+
+            .red-cell {
+                color: #D43A48;
+            }
+
+            tr {
+                background-color: white;
+            }
+
+            tbody tr:hover {
+                background-color: #ebdadd;
             }
 
             tr:nth-child(even) {
-                background-color: #dddddd;
+                background-color: white;
             }
-           
+            
             </style>
             <div class="glt-monitoring">
                 <div class="glt-wrapper-monitoring">
@@ -51,31 +79,23 @@ class GeoTechMonitoring extends HTMLElement {
                         <h3>GeoLegalTech</h3>
                     </div>
                 </div>
-                <div class="">
+                <div class="glt-monitoring-table">
                     <table>
                         <tr>
-                        <th></th>
-                        <th></th>
-                        <th></th>
-                        <th></th>
-                        <th></th>
-                        <th></th>
+                        <td>Fortschreibung Einzelhandelskonzept fuer die Stadt</td>
+                        <td>EHK</td>
+                        <td>Ratsitzung/Protokoll</td>
+                        <td>4200m</td>
+                        <td class="red-cell">Original-Dokument</td>
+                        <td class="red-cell">GLT Sharepoint</td>
                         </tr>
                         <tr>
-                        <td>Alfreds Futterkiste</td>
-                        <td>Maria Anders</td>
-                        <td>Germany</td>
-                        <td>Alfreds Futterkiste</td>
-                        <td>Maria Anders</td>
-                        <td>Germany</td>
-                        </tr>
-                        <tr>
-                        <td>Centro comercial Moctezuma</td>
-                        <td>Francisco Chang</td>
-                        <td>Mexico</td>
-                        <td>Alfreds Futterkiste</td>
-                        <td>Maria Anders</td>
-                        <td>Germany</td>
+                        <td>4. Aenderung des Bebauungsplanes Nr.5</td>
+                        <td>B</td>
+                        <td>Begruendung</td>
+                        <td>500m</td>
+                        <td class="red-cell">Original-Dokument</td>
+                        <td class="red-cell">GLT Sharepoint</td>
                         </tr>
                     </table>
                 </div>
